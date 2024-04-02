ff588bfc41a1198981bc494118424ee5a8050426
thekill022 <gibfathoni@gmail.com>
2024-04-02 12:40:32 +0800

    menyertakan library string dan membuat struck DetailAlamat dan struck Mahasiswa

--- Struck4/Struck4.cpp
+++ Struck4/Struck4.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+#include <string> //menyertakan library string untuk menggunakan fungsi getline
 using namespace std;
+
+//membuat struck DetailAlamat dan Mahasiswa untuk menyimpan input data mahasiswa
+struct DetailAlamat
+{
+    string desa;
+    string kota;
+};
+
+struct Mahasiswa
+{
+    string nim;
+    string nama;
+    DetailAlamat alamat;
+    int umur;
+};
+
 
 int main()
 {
