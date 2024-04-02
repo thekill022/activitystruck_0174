b063359fcd61a63638479553fdb0653a2fcd995e
thekill022 <gibfathoni@gmail.com>
2024-04-02 12:33:40 +0800

    membuat struck DetailAlamat dan Mahasiswa untuk menyimpan input data mahasiswa

--- struct3/struct3.cpp
+++ struct3/struct3.cpp
@@ -2,6 +2,23 @@
 #include <string> //menyertakan library string untuk menggunakan fungsi getline
 using namespace std;
 
+
+//membuat struck DetailAlamat dan Mahasiswa untuk menyimpan input data mahasiswa
+struct DetailAlamat
+{
+    string desa;
+    string kota;
+};
+
+struct Mahasiswa
+{
+    string nim;
+    string nama;
+    DetailAlamat alamat;
+    int umur;
+};
+
+
 int main()
 {
 	
