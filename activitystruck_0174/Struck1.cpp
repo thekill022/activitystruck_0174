456b901a53f06fe32c1057eed615eac02936aa23
thekill022 <gibfathoni@gmail.com>
2024-04-02 12:11:47 +0800

    struct mahasiswa untuk mengambil input data mahasiswa

--- activitystruck_0174/Struck1.cpp
+++ activitystruck_0174/Struck1.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 
+struct Mahasiswa //struct mahasiswa untuk mengambil input data mahasiswa
+{
+    string nim;
+    string nama;
+    string alamat;
+    int umur;
+};
+
 int main()
 {
     
