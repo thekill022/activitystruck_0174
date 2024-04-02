2ef188b91255bcfd352f21f7f50bbf34fb6a1304
thekill022 <gibfathoni@gmail.com>
2024-04-02 12:50:04 +0800

    membuat looping for untuk menampilkan input data mahasiswa

--- Struck4/Struck4.cpp
+++ Struck4/Struck4.cpp
@@ -43,4 +43,9 @@ int main()
         cin.ignore(1, '\n');
         cout << endl;
     }
+
+    for (int i = 0; i < 3; i++) //membuat looping for untuk menampilkan input data mahasiswa
+    {
+
+    }
 }
